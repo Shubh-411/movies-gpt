cce0262fcb5b970d018d4c3d789ef82c14ceed73
Shubham <Shubhamshukla813@gmail.com>
2025-06-11 19:45:24 +0530

    vite and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="font-bold text-red-500 text-3xl">
+        Welcome to my vite react app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
